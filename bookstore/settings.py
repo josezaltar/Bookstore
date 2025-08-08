404c0fc0f2b4262a7611e644de88616b149e755e
José Zaltar <josezaltar@email.com>
2025-08-08 13:29:37 -0300

    Aula 2: Introdução a paginação em Django Rest Framework

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
