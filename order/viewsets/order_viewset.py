167ac26d1ca6f31b184c0d45d9104670792f7ab4
José Zaltar <josezaltar@email.com>
2025-08-11 14:36:03 -0300

    Aula 7: Testes de segurança em Django Rest Framework

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -5,5 +5,5 @@ from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    queryset = Order.objects.all().order_by("id")
